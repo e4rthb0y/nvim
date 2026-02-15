f4868d63b2d3c9ca02d0a207b8f52622476b7dbd
Augusto Gallegos <omargallegos997@gmail.com>
2026-02-14 20:00:30 -0600

    feat(nvim/auto-dark-mode): enable automatic theme toggle based on system appearance

---
+++ lua/plugins/auto-dark-mode.lua
@@ -0,0 +1,15 @@
+return {
+    'f-person/auto-dark-mode.nvim',
+    opts = {
+        set_dark_mode = function()
+            local themery = require('themery')
+            themery.setThemeByName('catppuccin macchiato', true)
+        end,
+        set_light_mode = function()
+            local themery = require('themery')
+            themery.setThemeByName('catppuccin latte', true)
+        end,
+        update_interval = 3000,
+        fallback = 'dark',
+    },
+}
