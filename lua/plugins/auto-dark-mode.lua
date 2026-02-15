2f86964a75c78bf9e8859337907c7604aaa7f48c
Augusto Gallegos <omargallegos997@gmail.com>
2026-02-14 21:15:30 -0600

    chore(nvim/auto-dark-mode): change automatic light/dark themes

--- lua/plugins/auto-dark-mode.lua
+++ lua/plugins/auto-dark-mode.lua
@@ -1,13 +1,13 @@
+local themery = require('themery')
+
 return {
     'f-person/auto-dark-mode.nvim',
     opts = {
         set_dark_mode = function()
-            local themery = require('themery')
             themery.setThemeByName('catppuccin macchiato', true)
         end,
         set_light_mode = function()
-            local themery = require('themery')
-            themery.setThemeByName('catppuccin latte', true)
+            themery.setThemeByName('tokyonight day', true)
         end,
         update_interval = 3000,
         fallback = 'dark',
