7890d86be2abf63944d7aae79c68665453d956d5
Augusto Gallegos <omargallegos997@gmail.com>
2026-02-19 13:53:24 -0600

    feat(nvim/plugins/markdown-preview): configure auto-close and add toggle keymap
    - Set `mkdp_auto_close` to 1 to close preview when buffer is closed.
    - Add `<leader>mdp` keymap to toggle markdown preview.
    - Update build command to use `mkdp#util#install()`.
    - Define lazy-loading commands and filetypes.

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,9 +1,18 @@
 return {
     'iamcco/markdown-preview.nvim',
-    build = 'cd app && npm install',
-    config = function()
-        vim.g.mkdp_filetypes = { 'markdown' }
-        vim.g.mkdp_theme = 'light'
-    end,
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     ft = { 'markdown' },
+    build = function()
+        vim.fn['mkdp#util#install']()
+    end,
+    keys = {
+        {
+            '<leader>mdp',
+            '<cmd>MarkdownPreviewToggle<cr>',
+            desc = 'Markdown Preview',
+        },
+    },
+    config = function()
+        vim.g.mkdp_auto_close = 1
+    end,
 }
