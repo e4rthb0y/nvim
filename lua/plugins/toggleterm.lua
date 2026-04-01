78328c633ba7ed6ee6de98479a3e3178598357ec
Augusto Gallegos <omargallegos997@gmail.com>
2026-03-31 19:57:37 -0600

    fix: clear Ctrl-l sequence in terminal mode to "clean" terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,5 +1,11 @@
 local function config()
     require('toggleterm').setup()
+    vim.api.nvim_create_autocmd('TermOpen', {
+        pattern = 'term://*toggleterm#*',
+        callback = function()
+            vim.keymap.set('t', '<C-l>', [[<C-l>]], { buffer = true })
+        end,
+    })
 end
 
 return { 'akinsho/toggleterm.nvim', version = '*', config = config }
