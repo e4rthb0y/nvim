b03287f9cb84959fdde8fe65286297bd0bef5bde
Augusto Gallegos <omargallegos997@gmail.com>
2026-04-26 00:55:49 -0600

    feat(toggleterm): add keymaps for toggling and navigating terminals

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,11 +1,32 @@
 local function config()
-    require('toggleterm').setup({ direction = 'float' })
+    require('toggleterm').setup({
+        direction = 'float',
+        open_mapping = [[<leader>t]],
+    })
+
     vim.api.nvim_create_autocmd('TermOpen', {
         pattern = 'term://*toggleterm#*',
         callback = function()
-            vim.keymap.set('t', '<C-l>', [[<C-l>]], { buffer = true })
+            local opts = { buffer = 0 }
+            vim.keymap.set('t', '<C-l>', [[<C-l>]], opts)
+
+            vim.keymap.set('t', '<Tab>', [[<C-\><C-n>:TermNext<CR>]], opts)
+            vim.keymap.set('t', '<S-Tab>', [[<C-\><C-n>:TermPrev<CR>]], opts)
         end,
     })
+
+    vim.keymap.set(
+        'n',
+        '<Tab>',
+        '<cmd>TermNext<CR>',
+        { desc = 'Next Terminal' }
+    )
+    vim.keymap.set(
+        'n',
+        '<S-Tab>',
+        '<cmd>TermPrev<CR>',
+        { desc = 'Previous Terminal' }
+    )
 end
 
 return { 'akinsho/toggleterm.nvim', version = '*', config = config }
