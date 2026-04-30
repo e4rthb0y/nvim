7607b26e0c80140799492ee7c2ab06093fad2116
Augusto Gallegos <omargallegos997@gmail.com>
2026-04-29 19:35:56 -0600

    feat: add dynamic provider resolution and custom health checks

--- lua/config/providers.lua
+++ lua/config/providers.lua
@@ -1,14 +1,30 @@
+local function has(cmd)
+    return vim.fn.executable(cmd) == 1
+end
+
 -- Python
-local python_path = vim.fn.exepath('python3')
-if python_path ~= '' then
-    vim.g.python3_host_prog = python_path
+if has('python3') then
+    vim.g.python3_host_prog = vim.fn.exepath('python3')
+else
+    vim.g.loaded_python3_provider = 0
 end
 
 -- Ruby
-vim.g.loaded_ruby_provider = 0
+if not has('ruby') then
+    vim.g.loaded_ruby_provider = 0
+end
 
 -- Node
-vim.g.loaded_node_provider = 0
+if not has('node') then
+    vim.g.loaded_node_provider = 0
+end
 
 -- Perl
-vim.g.loaded_perl_provider = 0
+if not has('perl') then
+    vim.g.loaded_perl_provider = 0
+end
+
+-- System tools
+vim.g.has_rg = has('rg')
+vim.g.has_fd = has('fd')
+vim.g.has_fzf = has('fzf')
