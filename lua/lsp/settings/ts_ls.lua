f68000e85ea45375e40fc692445da62fd1091dec
Augusto Gallegos <omargallegos997@gmail.com>
2026-04-30 20:28:41 -0600

    feat(lsp): add denols and configure ts_ls to avoid conflicts

--- lua/lsp/settings/ts_ls.lua
+++ lua/lsp/settings/ts_ls.lua
@@ -1,4 +1,14 @@
+local util = require('lspconfig.util')
+
 return {
+    root_dir = function(fname)
+        local deno_root = util.root_pattern('deno.json', 'deno.jsonc')(fname)
+        if deno_root then
+            return nil
+        end
+        return util.root_pattern('package.json', 'tsconfig.json', 'jsconfig.json', '.git')(fname)
+    end,
+    single_file_support = false,
     settings = {
         typescript = {
             inlayHints = {
