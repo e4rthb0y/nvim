66d1085292fece75642a74c21b61e9008042f950
Augusto <3559837+augusto-agent[bot]@users.noreply.github.com>
2026-05-10 23:37:27 -0600

    style: fix formatting with stylua

--- lua/lsp/settings/vtsls.lua
+++ lua/lsp/settings/vtsls.lua
@@ -3,7 +3,8 @@ local util = require('lspconfig.util')
 return {
     root_dir = function(fname)
         local deno_root = vim.fs.root(fname, { 'deno.json', 'deno.jsonc' })
-        local node_root = vim.fs.root(fname, { 'package.json', 'tsconfig.json' })
+        local node_root =
+            vim.fs.root(fname, { 'package.json', 'tsconfig.json' })
 
         if deno_root and node_root then
             return (#node_root >= #deno_root) and node_root or nil
