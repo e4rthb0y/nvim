16a07419d8e843882f90560ce3f35aa780e49aba
Augusto <omargallegos997@gmail.com>
2026-05-14 15:06:47 -0600

    feat(lsp): add type definition for LSP root handler

--- lua/lsp/settings/jsonls.lua
+++ lua/lsp/settings/jsonls.lua
@@ -1,4 +1,5 @@
 return {
+    ---@type LSPRootHandler
     root_dir = function(bufnr, callback)
         local fname = vim.api.nvim_buf_get_name(bufnr)
         if fname:match('%.template$') then
--- lua/lsp/settings/lemminx.lua
+++ lua/lsp/settings/lemminx.lua
@@ -1,6 +1,5 @@
 return {
-    -- @param bufnr integer
-    -- @param callback function
+    ---@type LSPRootHandler
     root_dir = function(bufnr, callback)
         local fname = vim.api.nvim_buf_get_name(bufnr)
         local root = vim.fs.root(fname, { 'pom.xml', 'build.gradle', '.git' })
